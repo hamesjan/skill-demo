b98f1751beac7016e1e05443def21bc74f4aa783
hamesjan <jhan31903@gmail.com>
2022-02-01 13:28:04 -0800

    init

---
+++ Practice.java
@@ -0,0 +1,5 @@
+public class Practice {
+    public static int add(int one, int two) {
+        return one + two;
+    }
+}
---
+++ PracticeTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class PracticeTest {
+    @Test
+    public void testAdd() {
+        assertEquals(2, Practice.add(1, 2));
+    }
+
+}
